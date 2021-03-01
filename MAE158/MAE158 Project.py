550bb1cf646b0256eca9e01ab339d5a2ed4cec4e
Thomas_Slagle <tslagle@uci.edu>
2021-02-28 16:50:52 -0800

    Reorganized files, Started 158 Project

---
+++ MAE158/MAE158 Project.py
@@ -0,0 +1,180 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+#Functions
+def Re(R,L):
+    return (R*L)
+
+def C_th(C_r, sigma):
+    return C_r * sigma
+
+def MAC_exp(Y,C_r,sigma,B):
+    C_t = C_th(C_r, sigma)
+    C_r_fuse = C_r - (C_r - C_t)*Y/(B/2)
+    sigma_exp = C_t/C_r_fuse
+    return (2/3*C_r_fuse*(1+sigma_exp-sigma_exp/(1+sigma_exp)))
+
+def MAC(C_r,sigma):
+    return (2/3*C_r*(1+sigma-sigma/(1+sigma)))
+
+#Parasitic Drag Equation (Calc from Nabil Discussion)
+def c_f(R):
+    return  0.1944*R**-0.2734 + 0.0009782
+
+#e_lambda_zero Equation (calc from Nabil Discussion)
+def e(Ar, C_d_p):
+    return 0.9423 + 0.00041*Ar + 4.591*C_d_p - 6.878e-06*Ar**2 - 1.348*Ar*C_d_p - 242.9 *C_d_p**2 + 0.01989*Ar**2*C_d_p + 11.86*Ar*C_d_p**2 + 3483*C_d_p**3
+
+def kappa(M, Lambda, TC):
+    z = ((2-M**2)*np.cos(np.radians(Lambda)))/(np.sqrt(1 - M**2*np.cos(np.radians(Lambda))))
+    return 1 + z*TC + 100*TC**4
+
+#Given Parameters
+
+#General Plane Properties
+W_i = 97000 #lbs
+W_d = 82000 #lbs
+sfc = 0.82 #lb/lb-h
+W_ker = 6.7 #lb/gal
+
+#Wing Geometry
+Lambda_w = 24.5 #deg
+tc_w = 0.106
+b_w = 93.2 #ft
+S_w = 1000 #ft2
+sigma_w = 0.2
+c_r_w = 17.8 #ft
+S_covered_w = 0.17
+
+#Vertical Tail
+Lambda_v = 43.5 #deg
+tc_v = 0.09
+S_v = 161 #ft2
+sigma_v = 0.80
+c_r_v = 15.5
+
+#Horizontal Tail
+Lambda_h = 31.6 #deg
+tc_h = 0.09
+S_h = 261 #ft2
+sigma_h = 0.35
+c_r_h = 11.1 #ft
+
+#Fuselage Geometry
+l = 107 #ft
+dia = 11.5 #ft
+S_wet = 3280 #ft2
+
+#Pylons Geometry
+S_wet_p = 117 #ft2
+tc_p = 0.06
+Lambda_p = 0 #deg
+sigma_p = 1.0
+c_p = 16.2 #ft
+
+#Nacelles Geometry
+S_wet_n = 455 #ft2
+fin_ratio = 5.0
+L_n = 16.8 #ft
+
+#Flap hinge Fairing
+delta_f_FH = 0.15 #ft2
+
+#Environmental Variables
+h = 31000 #ft
+#M = 0.78
+T = -60 #F
+rho = 0.000876 #slugs/ft3
+mu = 3.04e-07
+gamma = 1.4
+R = 1718
+a = np.sqrt(gamma*R*(T+459.67))
+#V_0 = M*a
+V_array_kts = np.linspace(200,500,1000)
+D_i_array = np.array([])
+D_p_array = np.array([])
+D_tot_array = np.array([])
+ld_array = np.array([])
+
+i = 0
+for i in range (len(V_array_kts)):
+    V = V_array_kts[i]*1.68781
+    M = V/a
+    q = (rho*V**2)/2
+    #q = gamma/2*601.61*M**2
+    Re_l = rho*V/mu #Rn/ft
+
+    #Wing
+    mac_w = MAC_exp(dia, c_r_w, sigma_w, b_w)
+    re_w = Re(Re_l, mac_w)
+    cf_w = c_f(re_w)
+    kappa_w = kappa(M, Lambda_w, tc_w)
+    delta_f_w = (1.02*2*S_w*(1-S_covered_w)*cf_w*kappa_w)
+
+    #Horizontal Tail
+    mac_h = MAC(c_r_h, sigma_h)
+    re_h = Re(Re_l, mac_h)
+    cf_h = c_f(re_h)
+    kappa_h = kappa(M, Lambda_h, tc_h)
+    delta_f_h = (1.02*2*S_h*cf_w*kappa_h)
+
+    #Vertical Tail
+    mac_v = MAC(c_r_v, sigma_v)
+    re_v = Re(Re_l, mac_v)
+    cf_v = c_f(re_v)
+    kappa_v = kappa(M, Lambda_v, tc_v)
+    delta_f_v = (1.02*2*S_v*cf_v*kappa_v)
+
+    #Pylons
+    re_p = Re(Re_l, c_p)
+    cf_p = c_f(re_p)
+    kappa_p = kappa(M, Lambda_p, tc_p)
+    delta_f_p = (S_wet_p*cf_p*kappa_p)
+
+    #Fuselage
+    re_f = Re(Re_l, l)
+    fin_F = l/dia
+    kappa_f = 1.11
+    cf_f = c_f(re_f)
+    delta_f_f = S_wet*cf_f*kappa_f
+
+    #Nacelles
+    re_n = Re(Re_l, L_n)
+    cf_n = c_f(re_n)
+    kappa_n = 1.29
+    delta_f_n = S_wet_n*cf_n*kappa_n
+
+    #Total Parasite drag
+    f = 1.10*(delta_f_w + delta_f_f + delta_f_h + delta_f_v + delta_f_n + delta_f_p + delta_f_FH)
+    C_d_p = f/S_w
+
+    AsR = b_w**2/S_w
+
+    C_l = W_i/(q*S_w)
+    #print(C_l)
+
+    C_d_i = C_l**2/(np.pi*AsR*e(AsR, C_d_p))
+
+    D_i = (W_i/b_w)**2/(np.pi*q*e(AsR, C_d_p))
+    D_i_array = np.append(D_i_array,[D_i])
+    D_p = f*q
+    D_p_array = np.append(D_p_array,[D_p])
+
+    D_tot = D_p + D_i
+    D_tot_array = np.append(D_tot_array,[D_tot])
+
+    ld = D_tot/W_i
+    ld_array = np.append(ld_array, [ld])
+
+
+plt.plot(V_array_kts, D_tot_array, label = 'Total Drag')
+plt.plot(V_array_kts, D_i_array, label = 'Induced Drag')
+plt.plot(V_array_kts, D_p_array, label = 'parasitic Drag')
+plt.xlabel('Airspeed (Kts)')
+plt.ylabel('Drag (lbs)')
+plt.title('Drag v Airspeed')
+plt.legend()
+plt.show()
+
+
+
